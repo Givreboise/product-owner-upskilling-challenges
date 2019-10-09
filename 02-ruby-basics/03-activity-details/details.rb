888dc443390435bc8902363e5df4b5bf7de46d27
givreboise <chloestch@gmail.com>
2019-10-09 14:53:21 +0000

    Details Adventure Activity

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -1,5 +1,15 @@
 activity = {
-  # TODO: gather the characteristics of your preferred activity
+  "name" => "Escape Game",
+  "category" => "[ADVENTURE]",
+  "city" => "Nantes",
+  "duration" => 60,
+  "maxpax" => 4,
+  "price" => 120,
+
 }
 
-# TODO: your code goes here
+puts "#{activity["name"]}" "#{activity["category"]}"
+puts "Located in " "#{activity["city"]}"
+puts "Duration of " "#{activity["duration"]}" " minutes"
+puts "For " "#{activity["maxpax"]}" " participants"
+puts "Charged " "#{activity["price"]}" " €"
