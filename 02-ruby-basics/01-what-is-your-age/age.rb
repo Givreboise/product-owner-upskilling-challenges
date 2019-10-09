4858772b2a4b3e3384f3dc7ac0f6345570f5241e
givreboise <chloestch@gmail.com>
2019-10-09 13:51:42 +0000

    Asking name and year of birth

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -3,4 +3,10 @@ name = gets.chomp # asks user to type something in the Terminal
 
 puts "Ok got it!"
 
+puts "What is your year of birth?"
+year_of_birth = gets.chomp.to_i
+
+age = 2019 - year_of_birth
+
+puts "Hey #{name} ! You are #{age} year old this year."
 # TODO: your code goes here
