c1c5f413501b8d7e7ecd6c09fcac21f8f02563d7
givreboise <chloestch@gmail.com>
2019-10-09 14:30:35 +0000

    Asking new activity

--- 02-ruby-basics/02-listing-categories/categories.rb
+++ 02-ruby-basics/02-listing-categories/categories.rb
@@ -1,5 +1,16 @@
-categories = [
-  # TODO: gather the categories of activity
-]
+categories = ["Adventure", "Sport", "Visits", "Workshops", "Cultural", "Food"]
 
-# TODO: your code goes here
+puts "Which activity would you like?"
+newcategorie = gets.chomp
+
+categories << newcategorie
+
+categories = categories.sort
+
+puts "[CATEGORIES OF ACTIVITIES]"
+
+categories.each do |activity|
+  puts "#{activity}"
+end
+
+puts "Total: #{categories.size}"
