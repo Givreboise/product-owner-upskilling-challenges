edef4db9c1fb6a416a54a7cfcd007316c790bde8
givreboise <chloestch@gmail.com>
2019-10-11 15:24:09 +0000

    Fix comments on exercise 2 - API Ruby

--- 05-api-consumption/01-ruby-program/users_by_role.rb
+++ 05-api-consumption/01-ruby-program/users_by_role.rb
@@ -2,8 +2,7 @@ require "json"
 require "rest-client"
 
 puts "What is the role of the users you're looking for? (e.g. Figgo - Manager)"
-# role = gets.chomp # asks user to type something in the Terminal
-role = "Figgo - Manager"
+role = gets.chomp # asks user to type something in the Terminal
 
 puts "Ok got it! Let's search for the users..."
 
