c0151f53f1abcd1840cdffbce37ed96426aafd8c
givreboise <chloestch@gmail.com>
2019-10-14 13:38:17 +0000

    Adding button style and colors

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -6,35 +6,66 @@ enable :static
 
 get "/" do
   @activities = [
-      {"name" => "Escape Game",
-      "category" => "Adventure",
+      {"name" => "Knitting session",
+      "category" => "DIY",
       "city" => "Nantes",
       "duration" => 60,
       "maxpax" => 4,
       "price" => 120,
-      "img" => "https://images.unsplash.com/photo-1548412576-d92cb3275e23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80"
+      "img" => "https://images.unsplash.com/photo-1541944743827-e04aa6427c33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=626&q=80"
 
       },
+
+      {"name" => "Rafting",
+        "category" => "Adventure",
+        "city" => "Vezelay",
+        "duration" => 120,
+        "maxpax" => 10,
+        "price" => 80,
+        "img" => "https://images.unsplash.com/photo-1530866495561-507c9faab2ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=769&q=80"
+
+      },
+
+      {"name" => "Tasting gourmet teas",
+        "category" => "Food",
+        "city" => "London",
+        "duration" => 90,
+        "maxpax" => 15,
+        "price" => 300,
+        "img" => "https://images.unsplash.com/photo-1521012012373-6a85bade18da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80"
+
+        },
 
       {"name" => "Wine Testing",
-      "category" => "Food",
-      "city" => "Paris",
-      "duration" => 90,
-      "maxpax" => 15,
-      "price" => 300,
-      "img" => "https://images.unsplash.com/photo-1510812431401-41d2bd2722f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+        "category" => "Food",
+        "city" => "Beaunes",
+        "duration" => 90,
+        "maxpax" => 15,
+        "price" => 300,
+        "img" => "https://images.unsplash.com/photo-1510812431401-41d2bd2722f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+
+        },
+
+      {"name" => "Escape Game",
+        "category" => "Adventure",
+        "city" => "Nantes",
+        "duration" => 60,
+        "maxpax" => 4,
+        "price" => 120,
+        "img" => "https://images.unsplash.com/photo-1548412576-d92cb3275e23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=753&q=80"
 
       },
 
-      {"name" => "Beer Festival",
+      {"name" => "Oktoberfest",
       "category" => "Food",
-      "city" => "Lille",
+      "city" => "Munich",
       "duration" => 120,
       "maxpax" => 10,
       "price" => 80,
-      "img" => "https://images.unsplash.com/photo-1532635241-17e820acc59f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1003&q=80"
+      "img" => "https://cdn.theatlantic.com/assets/media/img/photo/2019/09/oktoberfest-2019-photos-opening-wee-1/o01_1176125103-1/original.jpg"
 
       }
+
     ]
 
   erb :index
